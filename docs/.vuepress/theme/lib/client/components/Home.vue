f5d5ec0dbd0f49fd391a1829ea02ae620114676a
✨NeserCode <nesercode@gmail.com>
2022-09-11 15:28:48 +0800

    Update Home.vue

--- docs/.vuepress/theme/lib/client/components/Home.vue
+++ docs/.vuepress/theme/lib/client/components/Home.vue
@@ -3,45 +3,42 @@ import HomeContent from "./HomeContent.vue";
 import HomeFooter from "./HomeFooter.vue";
 import HomeHitokoto from "./HomeHitokoto.vue";
 
-import { ref, onMounted, onUnmounted } from "vue";
+import { ref, onMounted } from "vue";
+import { onBeforeRouteLeave } from "vue-router";
 import { debounce } from "ts-debounce";
 const minScroll = ref(120);
 const scrollDiff = ref(0);
 
+const infferListener = debounce(() => {
+  let scrolled = document.documentElement.scrollTop || document.body.scrollTop;
+  let computedTop = document.querySelector(
+    ".neser-theme-article-list"
+  ).offsetTop;
+  scrollDiff.value -= window.scrollY;
+  if (
+    scrolled > minScroll.value &&
+    scrolled <= computedTop &&
+    scrollDiff.value < 0
+  )
+    window.scrollTo({
+      top: computedTop,
+      behavior: "smooth",
+    });
+
+  scrollDiff.value = window.scrollY;
+}, 300);
+
 function toggleListener(bool) {
-  window.addEventListener(
-    "scroll",
-    bool
-      ? debounce(() => {
-          let scrolled =
-            document.documentElement.scrollTop || document.body.scrollTop;
-          let computedTop = document.querySelector(
-            ".neser-theme-article-list"
-          ).offsetTop;
-          scrollDiff.value -= window.scrollY;
-          console.log(scrolled, computedTop, scrollDiff.value);
-          if (
-            scrolled > minScroll.value &&
-            scrolled <= computedTop &&
-            scrollDiff.value < 0
-          ) {
-            window.scrollTo({
-              top: computedTop,
-              behavior: "smooth",
-            });
-            console.log("?");
-          }
-          scrollDiff.value = window.scrollY;
-        }, 300)
-      : () => {}
-  );
+  if (bool) window.addEventListener("scroll", infferListener);
+  else window.removeEventListener("scroll", infferListener);
 }
 
 onMounted(() => {
   toggleListener(true);
 });
-onUnmounted(() => {
+onBeforeRouteLeave(() => {
   toggleListener(false);
+  console.log("watch");
 });
 </script>
 
